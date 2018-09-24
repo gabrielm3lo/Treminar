f0891c37a2c824827038b8d6d02564b9b7c93f56
gabrielm3lo <gabriel-melonogueira@hotmail.com>
2018-09-24 12:02:37 -0300

    Terminar o dia que Deus quiser

---
+++ laços.js
@@ -0,0 +1,33 @@
+// let salario;
+// salario = prompt("Sua Renda");
+// let idade; 
+// idade = prompt("Sua idade");
+// let emprestimo;
+// emprestimo = prompt ("Valor do imprestimo");
+// if (idade > 21 && idade <55){
+//     console.log("Idade Aprovado");
+//     if(salario*10<emprestimo){
+//         console.log ("negado");
+//     }
+//     else{
+//         console.log("Parabéns você foi aprovado");
+//     }
+// }
+// else {
+//     console.log("Desculpa, não foi possivel");
+// }
+
+
+let Numero = 17 ;
+let Palpite = prompt ("Digite um número de 1 a 50");
+
+while(Palpite == Numero){
+if(Palpite > Numero){
+    console.log( "Não deu match ");
+    }
+    else{(Palpite < Numero);
+    console.log ("Não deu match AZARADO");
+    }
+}
+//     else if(Palpite == numero );
+
